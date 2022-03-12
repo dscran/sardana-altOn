3023824a40b6700a2f3ef2d948b831924daee578
Michael Schneider <mschneid@mbi-berlin.de>
2022-03-12 23:11:23 +0100

    consolidate macros and hooks into two files

--- altOnMacros.py
+++ altOnMacros.py
@@ -116,3 +116,36 @@ def magnconf(self, moveable, ampl, waittime):
 
     self.setEnv('magnConf', magnConf)
 
+
+@macro()
+def alton(self):
+    """Macro alton"""
+    acqConf = self.getEnv('acqConf')
+    acqConf['altOn'] = True
+    self.setEnv('acqConf', acqConf)
+    self.info('switching alternate ON')
+
+    # enable minus field counters
+    mnt_grp = self.getObj(
+        self.getEnv('ActiveMntGrp'),
+        type_class=Type.MeasurementGroup)
+    counters = list(acqConf['counters'])
+    mnt_grp.setEnabled(True, *counters)
+
+
+@macro()
+def altoff(self):
+    """Macro altoff"""
+    acqConf = self.getEnv('acqConf')
+    acqConf['altOn'] = False
+    self.setEnv('acqConf', acqConf)
+    self.info('switching alternate OFF')
+
+    # disable minus field counters
+    mnt_grp = self.getObj(
+        self.getEnv('ActiveMntGrp'),
+        type_class=Type.MeasurementGroup)
+    counters = list(acqConf['counters'])
+    mnt_grp.setEnabled(False, *counters)
+
+
