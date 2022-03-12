9b7b70c9cf264a02225fc68c4d8b44596e6ba2ec
Michael Schneider <mschneid@mbi-berlin.de>
2022-03-12 13:02:38 +0100

    comment hard-coded magnetStatus

--- altOnHooks.py
+++ altOnHooks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 22 12:57:08 2018
+Created on 12.03.2022
 
 @author: MBI Berlin
 """
@@ -10,9 +10,9 @@ import os
 from PyTango import DeviceProxy
 
 @macro()
-def userPreAcq(self):
-    acqConf  = self.getEnv('acqConf')
-    altOn    = acqConf['altOn']
+def altOnHook(self):
+    acqConf = self.getEnv('acqConf')
+    altOn = acqConf['altOn']
     waittime = acqConf['waitTime']
 
     if waittime:
@@ -24,11 +24,11 @@ def userPreAcq(self):
         magnConf = self.getEnv('magnConf')
         ampl = magnConf['ampl']
         magwaittime = magnConf['waitTime']
-        magnet = self.getMotion(["magnet"])
-        magnetState = DeviceProxy('raremag/MagnetState/magnet')
+        magnet = self.getMotion([magnConf['moveable']])
+        # magnetState = DeviceProxy('raremag/MagnetState/magnet')
 
-        magnet.move(-1*ampl)
-        magnetState.magnet = -1*ampl
+        magnet.move(-ampl)
+        # magnetState.magnet = -ampl
 
         self.debug('mag. waiting for %.2f s', magwaittime)
         time.sleep(magwaittime)
@@ -36,11 +36,13 @@ def userPreAcq(self):
         parent = self.getParentMacro()
         if parent:
             integ_time = parent.integ_time
-            mnt_grp = self.getObj(self.getEnv('ActiveMntGrp'), type_class=Type.MeasurementGroup)
+            mnt_grp = self.getObj(
+                self.getEnv('ActiveMntGrp'),
+                type_class=Type.MeasurementGroup)
             state, data = mnt_grp.count(integ_time)
 
-        magnet.move(+1 * ampl)
-        magnetState.magnet = +1 * ampl
+        magnet.move(+ampl)
+        # magnetState.magnet = +1 * ampl
 
         self.debug('mag. waiting for %.2f s', magwaittime)
         time.sleep(magwaittime)
